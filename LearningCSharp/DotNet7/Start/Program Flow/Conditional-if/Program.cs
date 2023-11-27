c6d1efb3474d77f59d6e0adabba78d5ed8d66837
Zoey Baker <zoeybaker625@gmail.com>
2023-11-27 11:44:47 -0600

    initial commit

--- LearningCSharp/DotNet7/Start/Program Flow/Conditional-if/Program.cs
+++ LearningCSharp/DotNet7/Start/Program Flow/Conditional-if/Program.cs
@@ -13,6 +13,11 @@ namespace Conditionals
 
                 Console.WriteLine("theVal is 50");
             }
+            else if (theVal >= 51 && theVal <= 60 ) {
+                Console.WriteLine("theVal is between 51 and 60");
+            }
+
+
             else {
                 Console.WriteLine("theVal is something else");
             }
@@ -31,7 +36,7 @@ namespace Conditionals
             */
 
             // TODO: can be replaced by a ternary operator ?:
-
+            Console.WriteLine(theVal < 50 ? "theVal is small" : "theVal is large");
         }
     }
 }
